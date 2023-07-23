ac2a904f2739b59f452d640024e566ab3f34cc9e
Bruno Ferreira <contatobrunokye@gmail.com>
2023-07-23 12:58:09 -0300

    feat: requisito 08

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -15,8 +16,18 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        new_form = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        data = search_news({"timestamp": new_form})
+        news_list = []
+
+        for news in data:
+            form = (news["title"], news["url"])
+            news_list.append(form)
+
+        return news_list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
